e35193f69ad96c111840597093fb8cd361687673
volkovandr <andrey.v.volkov@gmail.com>
2018-01-04 22:17:02 +0100

    Considering the exit code of docker ps, not only the output

--- tester/src/main/scala/Tester.scala
+++ tester/src/main/scala/Tester.scala
@@ -1,6 +1,7 @@
 import com.typesafe.config.ConfigFactory
 import scala.concurrent.duration._
 import sys.process._
+import java.io._
 import scala.util.matching.Regex
 
 object Tester {
@@ -9,34 +10,56 @@ object Tester {
 		val composeFile = config.getString("compose-file")
 		println(s"Compose file is $composeFile")
 
-		startComposition(composeFile)
-		makeSureContainersHealthy(composeFile, 10 seconds)
-		shutdownComposition(composeFile)
+		startComposition(composeFile) 
+		try {
+			makeSureContainersHealthy(composeFile, 10 seconds)
+		} finally shutdownComposition(composeFile)
 	}
 
 	def startComposition(composeFile: String) = {
-		val up = (s"docker-compose -f $composeFile up -d" !)
+		print("Starting composition...")
+		val up = runCommand(s"docker-compose -f $composeFile up -d".split(" "))._1
 		if (up != 0) sys.error("Failed to bring up the Docker composition")
+		println("started")
 	}
 
 	def shutdownComposition(composeFile: String) = {
-		val down = (s"docker-compose -f $composeFile down" !)
+		print("Stopping the composition...")
+		val down = runCommand(s"docker-compose -f $composeFile down".split(" "))._1
 		if (down != 0) sys.error("Failed to shutdown the Docker composition")
+		println("stopped")
 	}
 
 	def makeSureContainersHealthy(composeFile: String, timeout: Duration) = {
-		val listCompose = (s"docker-compose -f $composeFile ps" !!)
-		val containers = listCompose.split("\n").toList.drop(2).map(_.split(" ")(0))
-		val parenthesis = ".+(\\()(.+)(\\)).+".r
+		val dockerComposePsResult = runCommand(s"docker-compose -f $composeFile ps".split(" "))
+		if(dockerComposePsResult._1 != 0) sys.error(s"`docker-compose ps` returned exit code ${dockerComposePsResult._1}")
+		val containersInDockerCompose = dockerComposePsResult._2.split("\n").toList.drop(2).map(_.split(" ")(0))
+		val parenthesisRegex = ".+(\\()(.+)(\\)).+".r
 		
 		val start = System.nanoTime()
-		while (("docker ps" !!).split("\n").toList.drop(1).filter(str => containers.exists(str contains _)).map(_ match {
-			case parenthesis(before, health, after) => Some(health)
-			case _ => None
-		}).flatten.filter(_ != "healthy").isEmpty == false) {
+		while ({
+			val dockerPsResult = runCommand("docker ps".split(" "))
+			if(dockerPsResult._1 != 0) sys.error(s"`docker ps` returned exit code ${dockerPsResult._1}")
+			
+			dockerPsResult._2.split("\n").toList.drop(1).
+			filter(dockerPsLine => containersInDockerCompose.exists(dockerPsLine contains _)).
+			map(_ match {case parenthesisRegex(_, health, _) => Some(health); case _ => None}).
+			flatten.filter(_ != "healthy").isEmpty == false
+		}) {
 			if((System.nanoTime() - start).nanoseconds > timeout) sys.error("Timeout waiting for containers to get healthy")
 			Thread.sleep(1000)
 		}
 	}
 
+	def runCommand(cmd: Seq[String]): (Int, String, String) = {
+  		val stdoutStream = new ByteArrayOutputStream
+  		val stderrStream = new ByteArrayOutputStream
+  		val stdoutWriter = new PrintWriter(stdoutStream)
+  		val stderrWriter = new PrintWriter(stderrStream)
+  		val exitValue = cmd.!(ProcessLogger(stdoutWriter.println, stderrWriter.println))
+  		stdoutWriter.close()
+  		stderrWriter.close()
+  		(exitValue, stdoutStream.toString, stderrStream.toString)
+	}
+	
 }
